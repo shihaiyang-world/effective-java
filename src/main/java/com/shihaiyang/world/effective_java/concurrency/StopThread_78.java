eeb6f232421178affb1d294b2e6d5d19d1815929
史海洋 <shihaiyang@shuidihuzhu.com>
2022-03-07 14:25:07 +0800

    78:同步访问共享的可变数据

--- src/main/java/com/shihaiyang/world/effective_java/concurrency/StopThread_78.java
+++ src/main/java/com/shihaiyang/world/effective_java/concurrency/StopThread_78.java
@@ -1,6 +1,7 @@
 package com.shihaiyang.world.effective_java.concurrency;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /*
 78:同步访问共享的可变数据
@@ -9,17 +10,32 @@ public class StopThread_78 {
     // 无法停止主线程，因为修改不可见
 //    private static boolean stopRequested;
     private static volatile boolean stopRequested;
+    private static AtomicLong atomicLong = new AtomicLong();
+    private static int i = 0;
+
+    private static long generateLong() {
+        return atomicLong.getAndIncrement();
+    }
 
     public static void main(String[] args) throws InterruptedException {
         Thread backgroundThread = new Thread(() -> {
-            int i = 0;
             while (!stopRequested) {
                 i++;
+                generateLong();
             }
         });
         backgroundThread.start();
-        TimeUnit.SECONDS.sleep(1);
+        Thread backgroundThread2 = new Thread(() -> {
+            while (!stopRequested) {
+                i++;
+                generateLong();
+            }
+        });
+        backgroundThread2.start();
+        TimeUnit.MILLISECONDS.sleep(1);
         stopRequested = true;
+        // 14299==11669
+        System.out.println(atomicLong.get() + "==" + i);
     }
 }
 
